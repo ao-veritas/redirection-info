70455413e0d5e5de61fcb0b0465e0877f0b0a74c
0xLPircy <taveesha.agarwal@gmail.com>
2024-10-11 15:58:26 +0530

    bento done

--- client/v3.0/src/components/Landing/FeatureCard.tsx
+++ client/v3.0/src/components/Landing/FeatureCard.tsx
@@ -1,11 +1,11 @@
-const FeatureCard = ({heading, text, flow, imag}:any) => {
+const FeatureCard = ({heading, text, flow, imag, imagClasses}:any) => {
   return (
-    <div className={`flex ${flow} justify-between items-center h-full text-[#212121]`}>
-        <img src={`/feat/${imag}.svg`} className='w-full h-full'/>
-        <div>
-            <h3 className='text-[21px] font-semibold tracking-tighter'>{heading}</h3>
-            <p className='text-[15px] tracking-wider leading-[20px] pt-[6px]'>{text}</p>
+    <div className={`flex ${flow} justify-between items-center h-full gap-[0px]`}>
+        <div className="w-full place-self-start">
+            <h3 className='text-[24px] font-semibold tracking-wider text-[#46B1BC]'>{heading}</h3>
+            <p className='text-[16.5px] tracking-widest leading-[24px] pt-[6px] text-[#ffffff] font-sans font-extralight'>{text}</p>
         </div>
+        <img src={`/feat/${imag}.svg`} className={`${imagClasses}`}/>
     </div>
   )
 }
--- client/v3.0/src/components/Landing/Features.tsx
+++ client/v3.0/src/components/Landing/Features.tsx
@@ -3,37 +3,54 @@ import FeatureCard from './FeatureCard'
 
 const Features = () => {
   return (
-    <section className='px-[90px] py-[90px] bg-[#212121] w-full min-h-[90vh]'>
-        <div className="grid lg:grid-cols-5 gap-[21px] mx-auto w-full">
-            <div className='col-span-2 grid grid-flow-row grid-rows-9 gap-[21px]'>
+    <section className='px-[150px] py-[90px] w-full min-h-[90vh]'>
+    <div className='flex flex-col gap-[21px]'>
+        <div className="grid lg:grid-cols-7 gap-[21px] mx-auto w-full">
                 <WobbleCard
-                containerClassName="h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px] row-span-5 p-0 shadow-[inset_0px_0px_16px_5px_#4a5568]"
-                className="py-[36px] "
+                containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-3 p-0"
+                className="py-[21px] innerFeatLinear rounded-[17.4px]"
                 >
-                    <FeatureCard heading="See Charts, Graphs and Analyse" text="Users can analyse projects on AO and Arweave with Onchain analysis, showing users, balances, messages and much more!" flow="flex-col" imag="1"/>
-                </WobbleCard>
-                <div className='grid grid-cols-2 gap-[21px] row-span-4'>
-                <WobbleCard
-                containerClassName="col-span-1 h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px]"
-                className="py-[36px]"
-                >
-                    <FeatureCard heading="Make streaks, earn AO" text="Earn AO based on streaks and usage!" flow="flex-col" imag="2"/>
+                    <FeatureCard heading="See Charts, Graphs and Analyse" text="Users can analyse projects on AO and Arweave with Onchain analysis, showing users, balances, messages and much more!" flow="flex-col" imag="1" imagClasses="h-[210px] w-[420px] place-self-end"/>
                 </WobbleCard>
                 <WobbleCard
-                containerClassName="col-span-1 h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px]"
-                className="py-[36px]"
+                containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-4 p-0"
+                className="py-[21px] innerFeatSlant rounded-[17.4px]"
                 >
-                    <FeatureCard heading="Predict on Projects" text="Predict on the outcome of milestones, progress and traffic of projects!" flow="flex-col" imag="3"/>
+                    <FeatureCard heading="Assets Assets and Assets" text="Earn AO based on streaks and usage!" flow="flex-col" imag="2" imagClasses="mb-[-24px] w-[510px] h-[210px] place-self-end"/>
+                </WobbleCard>
+                
+        </div>
+        <div className='grid lg:grid-cols-[repeat(16,_minmax(0,_1fr))] gap-[21px] mx-auto w-full'>
+                <div className='col-span-11 grid grid-cols-2 gap-[21px]'>
+                <WobbleCard
+                        containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-1 p-0"
+                        className="py-[21px] innerFeatLinear rounded-[17.4px]"
+                        >
+                            <FeatureCard heading="Make a streak with earn AO" text="Earn AO based on streaks and usage!" flow="flex-col" imag="3" imagClasses="mb-[-24px] mr-[-18px] w-[300px] h-[240px] mt-[45px] place-self-end"/>
+                </WobbleCard> 
+                <WobbleCard
+                        containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-1 p-0"
+                        className="py-[21px] innerFeatLinear rounded-[17.4px]"
+                        >
+                        <FeatureCard heading="Predict on Projects" text="Predict on the outcome of milestones, progress and traffic of projects!" flow="flex-col" imag="4" imagClasses="mb-[-24px] mr-[-24px] w-[270px] h-[210px] mt-[45px] place-self-end"/>
+                </WobbleCard>
+               
+                <WobbleCard
+                        containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-2 p-0"
+                        className="py-[21px] innerFeatSlant rounded-[17.4px]"
+                        >
+                        <FeatureCard heading="Track Your Investments" text="Your personalized user dashboard showcases all your yields, stakes and holdings!" flow="flex-row" imag="5" imagClasses="mb-[-39px] ml-[-30px] w-[375px] h-[270px] mt-[60px] place-self-end"/>
                 </WobbleCard> 
                 </div>
-            </div>
-            <div className='col-span-3 grid grid-flow-row grid-rows-9 gap-[21px]'>
-                <WobbleCard
-                containerClassName="col-span-1 lg:row-span-4 h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px]"
-                className="py-[36px]"
+            <WobbleCard
+                containerClassName="h-full min-h-[500px] featCardBg lg:min-h-[300px] col-span-5 p-0"
+                className="py-[21px] innerFeatLeaderboard rounded-[17.4px]"
                 >
-                    <FeatureCard heading="Assets Assets Assets" text="Lorem ipsum dolor sit amet" flow="flex-row" imag="4"/>
-                </WobbleCard>
+                    <FeatureCard heading="Leaderboards" text="Rise above in the leaderboard and flex your ranks on social media!" flow="flex-col" imag="6" imagClasses="place-self-center w-[270px] h-full"/>
+            </WobbleCard> 
+            </div>
+            {/* <div className='col-span-3 grid grid-flow-row grid-rows-9 gap-[21px]'>
+
                 <div className='row-span-5 grid grid-flow-col grid-cols-5 gap-[21px]'>
                     <WobbleCard
                     containerClassName="col-span-1 lg:col-span-3 h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px]"
@@ -41,15 +58,9 @@ const Features = () => {
                     >
                         <FeatureCard heading="Track your investmments" text="Your personalized user dashboard showcases all your yields, stakes and holdings!" flow="flex-col" imag="5"/>
                     </WobbleCard> 
-                    <WobbleCard
-                    containerClassName="col-span-1 lg:col-span-2 h-full bg-[#0E9C9C] min-h-[500px] lg:min-h-[300px]"
-                    className="py-[36px]"
-                    >
-                        <FeatureCard heading="Leaderboards" text="Rise above in the leaderboard and flex your ranks on social media!" flow="flex-col" imag="6"/>
-                    </WobbleCard> 
-                </div>
-            </div>
-        </div>
+
+            </div> */}
+    </div>
     </section>
   )
 }
