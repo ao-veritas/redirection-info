04cc542482d4d2cf20aae3095182811f5408350d
0xLPircy <taveesha.agarwal@gmail.com>
2024-10-12 12:45:27 +0530

    faq

--- client/v3.0/src/components/Landing/Faq.tsx
+++ client/v3.0/src/components/Landing/Faq.tsx
@@ -7,26 +7,27 @@ import {
 
 const Faq = () => {
   return (
-    <section className="w-full px-20 text-[30px] py-20">
-        <Accordion type="single" collapsible>
+    <section className="w-full text-[30px] pt-20 flex flex-col items-center">
+        <h1 className="bg-[#163437] border-[1.5px] border-[#46B1BC] rounded-full px-[60px] py-[6px] text-[#D9D9D9] mb-[60px]">FAQ</h1>
+        <Accordion type="single" collapsible className="w-[60%]">
             <AccordionItem value="item-1">
-                <AccordionTrigger className="text-[30px] text-[#46B1BC] font-medium px-6 py-[24px]">Is it accessible?</AccordionTrigger>
-                <AccordionContent className="text-[21px] text-[#46B1BC] font-light px-9">
-                Yes. It adheres to the WAI-ARIA design pattern.
+                <AccordionTrigger className="text-[24px] text-[#46B1BC] font-medium px-[60px]">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-[16.5px] leading-[30px] w-[90%] text-[#ffffff] tracking-widest font-thin px-[60px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-            <AccordionTrigger className="text-[30px] text-[#46B1BC] font-medium px-6 py-[24px]">Is it accessible?</AccordionTrigger>
-            <AccordionContent className="text-[21px] text-[#46B1BC] font-light px-9">
-                Yes. It adheres to the WAI-ARIA design pattern.
+                <AccordionTrigger className="text-[24px] text-[#46B1BC] font-medium px-[60px]">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-[16.5px] leading-[30px] w-[90%] text-[#ffffff] tracking-widest font-thin px-[60px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-            <AccordionTrigger className="text-[30px] text-[#46B1BC] font-medium px-6 py-[24px]">Is it accessible?</AccordionTrigger>
-            <AccordionContent className="text-[21px] text-[#46B1BC] font-light px-9">
-                Yes. It adheres to the WAI-ARIA design pattern.
+                <AccordionTrigger className="text-[24px] text-[#46B1BC] font-medium px-[60px]">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-[16.5px] leading-[30px] w-[90%] text-[#ffffff] tracking-widest font-thin px-[60px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionContent>
-            </AccordionItem>
+            </AccordionItem> 
         </Accordion>
     </section>
   )
