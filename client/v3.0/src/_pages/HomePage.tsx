a06954c847799214d3b2c394ce1f5c3934c5ba64
0xLPircy <taveesha.agarwal@gmail.com>
2024-10-15 18:19:50 +0530

    banner home page

--- client/v3.0/src/_pages/HomePage.tsx
+++ client/v3.0/src/_pages/HomePage.tsx
@@ -12,6 +12,30 @@ const HomePage = () => {
     <>
       <Navbar/>
       <main className={` bg-[#111111] font-raleway min-h-[100vh] w-full text-[#ffffff] flex flex-col justify-start items-center gap-6 overflow-hidden`}>
+{/* @ts-ignore */}
+<div id="sticky-banner" tabIndex="-1" className="fixed top-[90px] start-0 z-50 flex justify-between w-full p-4 bg-[#40959d15] backdrop-blur-[9px] rounded-full">
+    <div className="flex items-center mx-auto">
+        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+            <span className="inline-flex p-1 me-3 rounded-full bg-gray-600 w-6 h-6 items-center justify-center flex-shrink-0">
+                <svg className="w-3 h-3 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                    <path d="M15 1.943v12.114a1 1 0 0 1-1.581.814L8 11V5l5.419-3.871A1 1 0 0 1 15 1.943ZM7 4H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2v5a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V4ZM4 17v-5h1v5H4ZM16 5.183v5.634a2.984 2.984 0 0 0 0-5.634Z"/>
+                </svg>
+                <span className="sr-only">Light bulb</span>
+            </span>
+            <span>Analyse new projects and stay informed! <a href="https://flowbite.com" className="inline font-medium text-cyan-600 underline dark:text-cyan-500 underline-offset-2 decoration-600 dark:decoration-500 decoration-solid hover:no-underline">Go to Analysis Page</a></span>
+        </p>
+    </div>
+    <div className="flex items-center">
+        <button data-dismiss-target="#sticky-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span className="sr-only">Close banner</span>
+        </button>
+    </div>
+</div>
+
+
         <Hero/>
         <ScreenshotDisplay/>
         <Features/>
