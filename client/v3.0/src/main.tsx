e4d4c27b423e2df1a5aa5b7db2ad40f0624eb74e
Ashura707 <119521793+Ashura707@users.noreply.github.com>
2024-12-10 00:04:29 +0530

    implemented google analytics

--- client/v3.0/src/main.tsx
+++ client/v3.0/src/main.tsx
@@ -1,13 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import App from './App.tsx'
+// import './index.css';
+// import { ArweaveWalletKit } from 'arweave-wallet-kit'
+// import AppRouter from './AppRouter.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//          <ArweaveWalletKit
+//       config={{
+//         permissions: ["SIGN_TRANSACTION", "ACCESS_ADDRESS"],
+//         ensurePermissions: true,
+//       }}
+//       theme={{
+//         radius: "minimal",
+//         titleHighlight: { r: 209, g: 109, b: 27 },
+//         accent: { r: 37, g: 41, b: 28 },
+//       }}
+//     >
+//     <AppRouter />
+//     </ArweaveWalletKit>
+//   </StrictMode>,
+// )
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 import './index.css';
-import { ArweaveWalletKit } from 'arweave-wallet-kit'
-import AppRouter from './AppRouter.tsx'
+import { ArweaveWalletKit } from 'arweave-wallet-kit';
+import AppRouter from './AppRouter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-         <ArweaveWalletKit
+    <Helmet>
+      {/* Google Analytics Script */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-JGF465EZHY"></script>
+      <script type="text/javascript">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-JGF465EZHY');
+        `}
+      </script>
+    </Helmet>
+
+    <ArweaveWalletKit
       config={{
         permissions: ["SIGN_TRANSACTION", "ACCESS_ADDRESS"],
         ensurePermissions: true,
@@ -18,7 +57,7 @@ createRoot(document.getElementById('root')!).render(
         accent: { r: 37, g: 41, b: 28 },
       }}
     >
-    <AppRouter />
+      <AppRouter />
     </ArweaveWalletKit>
-  </StrictMode>,
-)
+  </StrictMode>
+);
