45fbebdc13b6cd95209010759007fed549570deb
0xLPircy <taveesha.agarwal@gmail.com>
2024-12-11 20:07:40 +0530

    gtag test

--- client/v3.0/src/main.tsx
+++ client/v3.0/src/main.tsx
@@ -1,27 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// // import App from './App.tsx'
-// import './index.css';
-// import { ArweaveWalletKit } from 'arweave-wallet-kit'
-// import AppRouter from './AppRouter.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//          <ArweaveWalletKit
-//       config={{
-//         permissions: ["SIGN_TRANSACTION", "ACCESS_ADDRESS"],
-//         ensurePermissions: true,
-//       }}
-//       theme={{
-//         radius: "minimal",
-//         titleHighlight: { r: 209, g: 109, b: 27 },
-//         accent: { r: 37, g: 41, b: 28 },
-//       }}
-//     >
-//     <AppRouter />
-//     </ArweaveWalletKit>
-//   </StrictMode>,
-// )
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Helmet } from 'react-helmet';
@@ -33,7 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Helmet>
       {/* Google Analytics Script */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-JGF465EZHY"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-6EB5E3GYRN"></script>
       <script type="text/javascript">
         {`
           window.dataLayer = window.dataLayer || [];
